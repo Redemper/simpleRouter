a5020e24c7fa69ffe02cc09ba876e292971f3a03
sivyer9303 <sivyer9303@163.com>
2021-10-21 16:27:17 +0800

    update gorm.yml and updte gorm conf

--- core/config/gormconfig.go
+++ core/config/gormconfig.go
@@ -1,55 +1,65 @@
 package config
 
 import (
-    "gopkg.in/yaml.v2"
-    "gorm.io/driver/mysql"
-    "gorm.io/gorm"
-    "io/ioutil"
-    "log"
-    "path/filepath"
-    "strings"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"strings"
 )
 
 type DBConf struct {
-    Dialector string `yaml:"dialector"`
-    Url string `yaml:"url"`
-    User string `yaml:"user"`
-    Password string `yaml:"password"`
-    Port string `yaml:"port"`
+	Dialector string `yaml:"dialector"`
+	Url       string `yaml:"url"`
+	User      string `yaml:"user"`
+	Password  string `yaml:"password"`
+	Port      string `yaml:"port"`
 }
 
 var iDB gorm.DB
 
-func init(){
-    // read conf
-    conf := new(DBConf)
-    path, errp := filepath.Abs("../../conf")
-    if errp == nil {
-        yamlFile, err := ioutil.ReadFile(path + "/gorm.yml")
-        if err != nil {
-            log.Fatalf("yamlFile.Get err #%v ", err)
-        }
-        err = yaml.Unmarshal(yamlFile, conf)
-        // err = yaml.Unmarshal(yamlFile, &resultMap)
-        if err != nil {
-            log.Fatalf("Unmarshal: %v", err)
-        }
-        //db, err := gorm.Open(mysql.Open(conf.generateDBString()), &gorm.Config{})
-
-        //iDB = gorm.Open(mysql.open(generateDBString()))
-    }
+func init() {
+	// read conf
+	conf := new(DBConf)
+	path, errp := filepath.Abs("../../conf")
+	if errp == nil {
+		yamlFile, err := ioutil.ReadFile(path + "/gorm.yml")
+		if err != nil {
+			log.Fatalf("yamlFile.Get err #%v ", err)
+		}
+		err = yaml.Unmarshal(yamlFile, conf)
+		// err = yaml.Unmarshal(yamlFile, &resultMap)
+		if err != nil {
+			log.Fatalf("Unmarshal: %v", err)
+		}
+		dbString := conf.generateDBString()
+		fmt.Println("db string =====  ", dbString)
+		db, err := gorm.Open(mysql.Open(dbString), &gorm.Config{})
+		db.AutoMigrate(&Router{})
+		//iDB = gorm.Open(mysql.open(generateDBString()))
+	}
 }
 
-func (conf *DBConf) generateDBString() string{
-    //var conStr = "cd_mall:Cd_Mall1@tcp(rm-2zek85dzv7g624hbz.mysql.rds.aliyuncs.com:3306)/mall_shop?charset=utf8mb4&parseTime=True&loc=Local"
-    var result strings.Builder
-    result.WriteString(conf.User)
-    result.WriteString(":")
-    result.WriteString(conf.Password)
-    result.WriteString("@tcp(")
-    result.WriteString(conf.Url)
-    result.WriteString(":")
-    result.WriteString(conf.Port)
-    result.WriteString(")/mall_shop?charset=utf8mb4&parseTime=True&loc=Local")
-    return result.String()
+type Router struct {
+	gorm.Model
+	Name      string `gorm:"name"`
+	OriginUri string `gorm:"origin_uri"`
+	Order     int    `gorm:"order"`
+}
+
+func (conf *DBConf) generateDBString() string {
+	//var conStr = "cd_mall:Cd_Mall1@tcp(rm-2zek85dzv7g624hbz.mysql.rds.aliyuncs.com:3306)/mall_shop?charset=utf8mb4&parseTime=True&loc=Local"
+	var result strings.Builder
+	result.WriteString(conf.User)
+	result.WriteString(":")
+	result.WriteString(conf.Password)
+	result.WriteString("@tcp(")
+	result.WriteString(conf.Url)
+	result.WriteString(":")
+	result.WriteString(conf.Port)
+	result.WriteString(")/mall_shop?charset=utf8mb4&parseTime=True&loc=Local")
+	return result.String()
 }
---
+++ go.mod
@@ -0,0 +1,9 @@
+module simpleRouter
+
+go 1.16
+
+require (
+	gopkg.in/yaml.v2 v2.4.0
+	gorm.io/driver/mysql v1.1.1
+	gorm.io/gorm v1.21.12
+)
