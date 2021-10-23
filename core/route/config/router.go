5e31c94675f48130edab3a91f1aaddc58b0ceb9c
sivyer9303 <sivyer9303@163.com>
2021-10-23 17:26:54 +0800

    添加字段

--- core/route/config/router.go
+++ core/route/config/router.go
@@ -10,6 +10,7 @@ var routerMap sync.Map
 type Router struct {
 	Name      string `gorm:"name",yaml:"name"`
 	OriginUri string `gorm:"origin_uri",yaml:"origin-uri"`
+	TargetUri string `gorm:"target_uri",yaml:"taget-uri"`
 	Order     int    `gorm:"order",yaml:"order"`
 	Enabled   bool   `gorm:"enabled",yaml:"enabled"`
 	gorm.Model
